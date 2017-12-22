4f7b700dc25b0b1c639a082737ce206e613fcafd
WolfeTyler <tbwolfe@live.com>
2017-12-22 15:02:38 -0500

    Update css styling on search results

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -40,15 +40,16 @@ class User extends React.Component {
         return (
             <div className="user-page">
                 <div className="user-info">
-                    <div className="container">
-                        <h2 className="user-info__title">B/L Number: TXG{user.id}</h2>
+                    <div>
+                        <h1 className="user-info__title">B/L Number: {user.booking_number}</h1>
                         <p className="user-info__bio"></p>
                         <p className="user-info__bio">Steamship Line: {user.steamship_line}</p>
                         <p className="user-info__bio">Origin: {user.origin}</p>
                         <p className="user-info__bio">Destination: {user.destination}</p>
                         <p className="user-info__bio">Vessel: {user.vessel}</p>
                         <p className="user-info__bio">Vessel ETA: {user.vessel_eta}</p>
-                        <div className="well">
+                        <p className="user-info__bio"></p>
+                        <div>
                           <p className="user-info__bio">Containers: {user.number}</p>
                           <p className="user-info__bio">Size: {user.size}</p>
                           <p className="user-info__bio">Type: {user.type}</p>
